cc7dc353a16f41ee1aefcc95cd9282f659ff9141
Fabian Wörenkämper <f.woerenkaemper@krombacher.de>
2025-05-02 16:37:55 +0200

    Implement code changes to enhance functionality and improve performance

--- train_ml_regression_model.py
+++ train_ml_regression_model.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+from dotenv import load_dotenv
+import http.client
+import urllib.parse
+import requests
+from prepare_data import get_merged_data_for_training, export_to_parquet
 from sklearn.model_selection import TimeSeriesSplit
 # from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.ensemble import RandomForestRegressor
@@ -12,6 +17,33 @@ import os
 import polars as pl
 from datetime import datetime, timedelta
 import plotly.express as px
+
+def send_pushover_notification_new(user, message):
+    load_dotenv()
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    pushover_api_token = os.getenv("PUSHOVER_TAPO_API_TOKEN")
+    conn.request("POST", "/1/messages.json",
+                 urllib.parse.urlencode({
+                     "token": pushover_api_token,
+                     "user": user,
+                     "message": message,
+                 }), {"Content-type": "application/x-www-form-urlencoded"})
+    conn.getresponse()
+    
+def send_pushover_notification_img(user, message, image_path="data/prediction_overproduction.png"):
+    load_dotenv()
+    pushover_api_token = os.getenv("PUSHOVER_TAPO_API_TOKEN")
+    
+    data = {
+        "token": pushover_api_token,
+        "user": user,
+        "message": message,
+    }
+
+    files = {"attachment": open(image_path, "rb")} if image_path else None
+
+    response = requests.post("https://api.pushover.net/1/messages.json", data=data, files=files)
+    response.raise_for_status() 
 
 def read_data(data_path):
     # Read the data from the parquet file
@@ -146,51 +178,116 @@ def get_forecast_data():
     return df_forecast
 
     
-def predict_tomorrow(model, weather_forecast, features_prediction_only):
+def predict_tomorrow(model, weather_forecast, features_prediction_only, plot_images=False):
 
     # Check what columns are actually available in the result
-    print("\nAvailable columns in new:", weather_forecast.columns.tolist())
+    print("\nAvailable columns:", weather_forecast.columns.tolist())
     available_features = [col for col in features_prediction_only if col in weather_forecast.columns]
     # Tomorrow in UTC
     tomorrow = datetime.now().date() + timedelta(days=1)
-    if 'time' in weather_forecast.columns and len(available_features) > 0:
-        fig = px.line(weather_forecast, x='time', y=available_features, 
-                    title=f"Weather forecast for {tomorrow}")
-        fig.show()
-    else:
-        print("Required columns not found in the dataframe. Available columns:", weather_forecast.columns)    
+    if plot_images:
+        if 'time' in weather_forecast.columns and len(available_features) > 0:
+            fig = px.line(weather_forecast, x='time', y=available_features, 
+                        title=f"Weather forecast for {tomorrow}")
+            fig.show()
+        else:
+            print("Required columns not found in the dataframe. Available columns:", weather_forecast.columns)    
     
     # get solar prediction with model
     weather_forecast["predicted"] = model.predict(weather_forecast[available_features])
     weather_forecast["predicted"] = weather_forecast["predicted"].clip(lower=0)
     # plot the prediction
-    if 'time' in weather_forecast.columns:
-        fig = px.line(weather_forecast, x='time', y='predicted', 
-                    title=f"Solar prediction for {tomorrow}")
-        fig.show()
-    else:
-        print("Date column not found in the dataframe. Available columns:", weather_forecast.columns)
+    if plot_images:
+        if 'time' in weather_forecast.columns:
+            fig = px.line(weather_forecast, x='time', y='predicted', 
+                        title=f"Solar prediction for {tomorrow}")
+            fig.show()
+        else:
+            print("Date column not found in the dataframe. Available columns:", weather_forecast.columns)
+    return weather_forecast
+
+
+def compute_enegery_overproduction(df, threshold=20000):
+    overproduction_df = df.copy()
+    # Compute the energy overproduction
+    overproduction_df["overproduction"] = overproduction_df["predicted"] - threshold
+    # what are suitable times to consume energy?
+    overproduction_df["overproduction"] = overproduction_df["overproduction"].clip(lower=0)
+    # get the time of the overproduction
+    overproduction_df["overproduction_time"] = overproduction_df["time"].dt.strftime("%Y-%m-%d %H:%M:%S")
+    overproduction_df = overproduction_df[overproduction_df["overproduction"] > 0]
+    
+    # Tomorrow in UTC
+    tomorrow = datetime.now().date() + timedelta(days=1)
+    # create a matplotlib plot where the x-axis is the time and the y-axis is the predicted value and the threshold is 20000.
+    # if the predicted value is greater than 20000, then the value is red, otherwise it is green so plot all in one plot
+    fig, ax = plt.subplots()
+    # Extract hour from datetime for x-axis
+    hours = df["time"].dt.hour
+    
+    ax.plot(hours, df["predicted"], label="Predicted", color="blue")
+    ax.axhline(y=threshold, color='r', linestyle='--', label="Threshold")
+    ax.fill_between(hours, threshold, df["predicted"], 
+                    where=(df["predicted"] > threshold), color='green', alpha=0.5)
+    ax.fill_between(hours, threshold, df["predicted"], 
+                    where=(df["predicted"] <= threshold), color='red', alpha=0.5)
+    ax.set_title(f"Solar prediction for {tomorrow}")
+    ax.set_xlabel("Hour of day")
+    ax.set_ylabel("Predicted Power (W)")
+    ax.grid(True, linestyle='--', alpha=0.7)  # Add grid lines
+    plt.legend()
+    # plt.show()
+    # Save the plot
+    plt.savefig("data/prediction_overproduction.png")
+    return overproduction_df[["overproduction_time", "overproduction"]].reset_index(drop=True)
+
     
     
 def main():
     # Read the data
     path = "data"
-    file = "merged_data_weather_power.parquet"
-    file_path = os.path.join(path, file)
-    data = read_data(file_path)
+    merged_data_file = "merged_data_weather_power.parquet"
+    merged_data_file_path = os.path.join(path, merged_data_file)
+    
+    update_data = False
+    get_power_from_db = False
+    get_weather_data_from_api = True
+    
+    update_model = True
+    if update_data:
+        # Get the merged data for training
+        merged_data = get_merged_data_for_training(get_power_from_db=get_power_from_db, get_weather_data_from_api=get_weather_data_from_api)
+        # export the data to parquet
+        export_to_parquet(merged_data, path)
+    else:
+        merged_data = read_data(merged_data_file_path)
     # Features
     features = ['radiation_global', 'radiation_sky_short_wave_diffuse', 
                 'sunshine_duration', 'temperature_air_mean_2m', 
                 'cloud_cover_total', 'humidity']
     # # Test the models
     # test_models(data, features)
-    model = train_model(data, features)
-    joblib.dump(model, "data/model_new.pkl")
+    if update_model:
+        # Train the model
+        model = train_model(merged_data, features)
+        # Save the model
+        joblib.dump(model, "data/model_weather_solar_power.pkl")
+    else:
+        # Load the model
+        model = joblib.load("data/model_weather_solar_power.pkl")
     # # test prediction
     # test_predict(model, data, features)
     weather_forecast = get_forecast_data()
     # predict tomorrow
-    predict_tomorrow(model, weather_forecast, features)
+    forecast_data = predict_tomorrow(model, weather_forecast, features)
+    # The predicted column is the energy production in W for the next day per hour
+    # if predicted value is > 20000 W then we produce more than we consume
+    df_overproduction = compute_enegery_overproduction(forecast_data, threshold=20000)
+    output_string = f"Overproduction for tomorrow: {df_overproduction.to_string(index=False)}"
+    print(output_string)
+    # send pushover notification
+    pushover_user_group = os.getenv("PUSHOVER_USER_GROUP_WOERIS")
+    send_pushover_notification_img(pushover_user_group, output_string, "data/prediction_overproduction.png")
     
 if __name__ == "__main__":
     main()
